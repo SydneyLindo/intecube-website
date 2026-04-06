bb29d977193d3037888d87be07a1f4d9fbab4d4f
SydneyLindo <sydneylindo24@gmail.com>
2026-04-06 18:35:37 +0200

    Create IntecubeLogo SVG component

---
+++ src/IntecubeLogo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IntecubeLogo = () => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 0L0 40H40L20 0Z" fill="#3b82f6"/>
+      </svg>
+      <span style={{ marginLeft: '8px', fontSize: '24px', color: '#3b82f6' }}>Intecube</span>
+    </div>
+  );
+};
+
+export default IntecubeLogo;
