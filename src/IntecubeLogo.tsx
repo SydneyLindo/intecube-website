c6bbf0e9b34379a26bb50ec4aff494443ef82fa3
Lindo Sydney <sydneylindo24@gmail.com>
2026-04-09 23:58:10 +0200

    Remove triangle from IntecubeLogo

--- src/IntecubeLogo.tsx
+++ src/IntecubeLogo.tsx
@@ -1,14 +1,3 @@
 import React from 'react';
-
-const IntecubeLogo = () => {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 0L0 40H40L20 0Z" fill="#3b82f6"/>
-      </svg>
-      <span style={{ marginLeft: '8px', fontSize: '24px', color: '#3b82f6' }}>Intecube</span>
-    </div>
-  );
-};
-
+const IntecubeLogo = () => <span style={{ fontSize: '24px', fontWeight: 'bold', color: '#0f172a' }}>Intecube</span>;
 export default IntecubeLogo;
